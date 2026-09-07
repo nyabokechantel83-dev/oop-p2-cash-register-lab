3576197d8cdc0238fad3cdacbbe20d91510affbb
chantel <nyabokechantel83@gmail.com>
2026-09-07 08:58:20 +0300

    Implement cash register functionality

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,57 @@
-#!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self, discount=0):
+        self._discount = 0
+        self.discount = discount
+
+        self.total = 0
+        self.items = []
+        self.previous_transactions = []
+
+    @property
+    def discount(self):
+        return self._discount
+
+    @discount.setter
+    def discount(self, value):
+        if isinstance(value, int) and 0 <= value <= 100:
+            self._discount = value
+        else:
+            print("Not valid discount")
+
+    def add_item(self, item, price, quantity=1):
+        self.total += price * quantity
+
+        for i in range(quantity):
+            self.items.append(item)
+
+        transaction = {
+            "item": item,
+            "price": price,
+            "quantity": quantity
+        }
+
+        self.previous_transactions.append(transaction)
+
+    def apply_discount(self):
+        if self.discount == 0:
+            print("There is no discount to apply.")
+            return
+
+        self.total = self.total - (self.total * self.discount / 100)
+
+        print(f"After the discount, the total comes to ${self.total:g}.")
+
+    def void_last_transaction(self):
+        if len(self.previous_transactions) == 0:
+            return
+
+        transaction = self.previous_transactions.pop()
+
+        item = transaction["item"]
+        price = transaction["price"]
+        quantity = transaction["quantity"]
+
+        self.total -= price * quantity
+
+        for i in range(quantity):
+            self.items.remove(item)
